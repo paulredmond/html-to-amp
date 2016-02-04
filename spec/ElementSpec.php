e8cf50153d9f9814e52bfdfc1777d45c431e21a1
Paul Redmond <paulrredmond@gmail.com>
2016-02-04 16:56:57 -0700

    Add More Coverage to the Element

--- spec/ElementSpec.php
+++ spec/ElementSpec.php
@@ -4,8 +4,9 @@ namespace spec\Predmond\HtmlToAmp;
 
 use DOMNode;
 use DOMDocument;
-use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use PhpSpec\ObjectBehavior;
+use Predmond\HtmlToAmp\Element;
 
 class ElementSpec extends ObjectBehavior
 {
@@ -26,12 +27,17 @@ class ElementSpec extends ObjectBehavior
         $this->shouldHaveType('Predmond\HtmlToAmp\ElementInterface');
     }
 
+    function it_responds_to_getNode()
+    {
+        $this->getNode()->shouldReturnAnInstanceOf('DOMElement');
+    }
+
     function it_can_get_the_tag_name()
     {
         $this->getTagName()->shouldReturn("div");
     }
 
-    public function it_can_get_the_element_value()
+    public function it_responds_to_getValue()
     {
         $this->getValue()->shouldReturn('Hello world!');
     }
@@ -40,6 +46,13 @@ class ElementSpec extends ObjectBehavior
     public function it_can_get_an_attribute()
     {
         $this->getAttribute('id')->shouldReturn('welcome');
+        $this->getAttribute('foo')->shouldReturn('');
+    }
+
+    public function it_can_set_an_attribute()
+    {
+        $this->setAttribute('foo', 'bar')->shouldReturn(null);
+        $this->getAttribute('foo')->shouldReturn('bar');
     }
 
     /** @test **/
@@ -107,5 +120,123 @@ class ElementSpec extends ObjectBehavior
 
         $this->beConstructedWith($node);
         $this->remove()->shouldReturn($node);
+    }
+
+    public function it_can_get_the_parent()
+    {
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $div = $doc->createElement('div');
+        $p = $doc->createElement('p', 'Hello World');
+        $div->appendChild($p);
+
+        $this->beConstructedWith($p);
+        $this->getParent()->shouldReturnAnInstanceOf('Predmond\HtmlToAmp\Element');
+    }
+
+    public function it_can_append_an_element(Element $element)
+    {
+
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $div = $doc->createElement('div');
+        $p = $doc->createElement('p', 'Hello World');
+
+        $element->getNode()->willReturn($p);
+
+        $this->beConstructedWith($div);
+
+        $this->appendChild($element)->shouldReturn(null);
+    }
+
+    public function it_can_create_a_new_element_from_the_owner_document()
+    {
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $node = $doc->createElement('div', 'Hello world!');
+        $this->beConstructedWith($node);
+
+        $p = $this
+            ->createWritableElement('p', ['class' => 'foo'])
+            ->shouldReturnAnInstanceOf(Element::class);
+
+        $this->appendChild($p)->shouldReturn(null);
+    }
+
+    public function it_can_replace_itself_in_the_document_with_another_element()
+    {
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $parent = $doc->createElement('div');
+        $container = $doc->createElement('div', 'hello world');
+        $parent->appendChild($container);
+
+        $this->beConstructedWith($container);
+
+        $p = $this
+            ->createWritableElement('p', ['class' => 'foo'])
+            ->shouldReturnAnInstanceOf(Element::class);
+
+        $this->replaceWith($p)->shouldReturnAnInstanceOf(\DOMElement::class);
+    }
+
+    public function it_should_not_replace_itself_if_a_parent_node_is_not_found()
+    {
+        $p = $this->createWritableElement('p');
+
+        $this->replaceWith($p)->shouldBe(false);
+    }
+
+    public function it_removes_itself_from_the_document()
+    {
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $parent = $doc->createElement('div');
+        $container = $doc->createElement('div', 'hello world');
+        $parent->appendChild($container);
+
+        $this->beConstructedWith($container);
+
+        $this->remove()->shouldReturn($this->getNode());
+    }
+
+    public function it_should_not_remove_itself_without_a_parent_node()
+    {
+        $this->remove()->shouldBe(false);
+    }
+
+    public function it_can_get_child_elements()
+    {
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $parent = $doc->createElement('div');
+        $container = $doc->createElement('div', 'hello world');
+        $parent->appendChild($container);
+
+        $this->beConstructedWith($parent);
+
+        $this->getChildren()->shouldContainElements();
+    }
+
+    public function it_should_not_return_child_elements_when_none_exist()
+    {
+        $node = (new DOMDocument('1.0', 'utf-8'))
+            ->createElement('div');
+
+        $this->beConstructedWith($node);
+        
+        $this->getChildren()->shouldNotContainElements();
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'containElements' => function (array $subject) {
+                $count = 0;
+                $total = count($subject);
+
+                foreach ($subject as $node) {
+                    if ($node instanceof Element) {
+                        $count++;
+                    }
+                }
+
+                return ($count === $total && $count > 0);
+            }
+        ];
     }
 }
